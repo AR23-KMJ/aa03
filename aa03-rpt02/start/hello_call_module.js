b5a142a6fb68b8e5de00cd2ffa79bf75e0ad0cd7
AR03-KMJ <55528705+AR23-KMJ@users.noreply.github.com>
2023-09-14 16:46:49 +0900

    aa03_rpt02

---
+++ aa03-rpt02/start/hello_call_module.js
@@ -0,0 +1,4 @@
+var olleh = require('./hello_user_module.js');
+
+olleh("Node.js");
+olleh("aa03");
---
+++ aa03-rpt02/start/hello_function.js
@@ -0,0 +1,8 @@
+// hello_function.js
+function hello(what) {
+    console.log("Hello " + what +" !");
+
+}
+
+hello("aa03");
+hello("AA23 , 김민재");
